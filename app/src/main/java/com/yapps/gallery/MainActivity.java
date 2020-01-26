ef05c6a3149e8d12619e54a78f94b0aa547aa7da
YoungWenMing <yangwenming99@sina.com>
2020-01-26 10:23:20 +0800

    add permissionRequest for second fragment

--- app/src/main/java/com/yapps/gallery/MainActivity.java
+++ app/src/main/java/com/yapps/gallery/MainActivity.java
@@ -1,18 +1,27 @@
 package com.yapps.gallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.yapps.gallery.easyloader.LoaderActivity;
 import com.yapps.gallery.imagewall.ui.ImageGridActivity;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 public class MainActivity extends Activity implements View.OnClickListener {
 
+    private final String TAG = "MainAct";
+    private final int EXTERNAL_STORAGE_REQUEST_CODE = 0x0;
 
     Button btnLoader, btnWall;
 
@@ -34,11 +43,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 startActivity(intent);
                 break;
             case R.id.wall_button:
-                Intent intent1 = new Intent(this, ImageGridActivity.class);
-                startActivity(intent1);
+                if (ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.READ_EXTERNAL_STORAGE )!= PackageManager.PERMISSION_GRANTED){
+                    requestStoragePermission();
+                }else {
+                    Intent intent1 = new Intent(this, ImageGridActivity.class);
+                    startActivity(intent1);
+                }
                 break;
             default:
                 break;
         }
+    }
+
+    public void requestStoragePermission() {
+        Log.i(TAG, "Request external storage permission directly!");
+        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_REQUEST_CODE);
     }
 }
