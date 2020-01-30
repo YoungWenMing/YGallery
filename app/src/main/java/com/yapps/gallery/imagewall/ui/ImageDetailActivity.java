5614837305c7f597e7e04df8ea68016aa0ba2590
YoungWenMing <yangwenming99@sina.com>
2020-01-30 14:12:21 +0800

    add image detail activity

---
+++ app/src/main/java/com/yapps/gallery/imagewall/ui/ImageDetailActivity.java
@@ -0,0 +1,116 @@
+package com.yapps.gallery.imagewall.ui;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+
+import com.yapps.gallery.R;
+import com.yapps.gallery.imagewall.util.CursorBitmapFetcher;
+import com.yapps.gallery.imagewall.util.ImageCache;
+
+public class ImageDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String INIT_IMAGE = "initial_image";
+
+    private CursorBitmapFetcher mFetcher;
+
+    private ViewPager mPager;
+
+    private ImagePageAdapter mAdapter ;
+
+    private int numPages;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_detail_main);
+
+        //TODO:find pointer of views
+        mPager = findViewById(R.id.pager);
+        final Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);   //make the toolbar act as an action bar
+
+        //TODO:adjust the view to screen,
+        final DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels, width = displayMetrics.widthPixels;
+        int longest = (height > width ) ? height : width;
+
+        //TODO:initialize the imageFetcher
+        mFetcher = new CursorBitmapFetcher(this, longest);
+        mFetcher.addImageCache(ImageCache.CacheLevel.HIGH_CACHE);
+
+        //TODO:set the view pager
+        numPages = mFetcher.getCount();
+        mAdapter = new ImagePageAdapter(getSupportFragmentManager(), numPages);
+        mPager.setAdapter(mAdapter);
+        mPager.setPageMargin((int) getResources().getDimension(R.dimen.horizontal_page_margin));
+
+        //set the action bar
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayShowTitleEnabled(false);    //hide the title
+            actionBar.setDisplayHomeAsUpEnabled(true);      //display back bottom
+            mPager.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    if ((visibility & View.SYSTEM_UI_FLAG_LOW_PROFILE) != 0){
+                        actionBar.hide();
+                    }else {
+                        actionBar.show();
+                    }
+                }
+            });
+
+            mPager.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+            actionBar.hide();
+        }
+
+        //the index of first item to show
+        int curItemNum = getIntent().getIntExtra(INIT_IMAGE, -1);
+        if (curItemNum != -1){
+            mPager.setCurrentItem(curItemNum);
+        }
+
+    }
+
+
+    private class ImagePageAdapter extends FragmentStatePagerAdapter {
+        private int mSize ;
+
+        ImagePageAdapter(FragmentManager fm, int count){
+            super(fm);
+            mSize = count;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return mSize;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int vis = mPager.getSystemUiVisibility();
+        if ((vis & View.SYSTEM_UI_FLAG_LOW_PROFILE) != 0 ){
+            mPager.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        }else {
+            mPager.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        }
+    }
+}
