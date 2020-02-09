5511517f306c1b8923107f392f748618ee153a91
YoungWenMing <yangwenming99@sina.com>
2020-02-09 10:34:09 +0800

    add gesture detector and scale animator

--- app/src/main/java/com/yapps/gallery/imagewall/view/DetailImageView.java
+++ app/src/main/java/com/yapps/gallery/imagewall/view/DetailImageView.java
@@ -2,6 +2,7 @@ package com.yapps.gallery.imagewall.view;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -9,12 +10,14 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.ScaleAnimation;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class DetailImageView extends AppCompatImageView {
@@ -24,23 +27,35 @@ public class DetailImageView extends AppCompatImageView {
     private View.OnClickListener mClickListener = null;
 
     /**
+     * 图片缩放动画时间
+     */
+    public static final int SCALE_ANIMATOR_DURATION = 200;
+
+    /**
      * 最大放大比例
      * @see #getMaxScale()
      */
-    private static final float MAX_SCALE = 4f;
+    private static final float MAX_SCALE = 2f;
 
     /**
      * 外层变换矩阵
-     * @see #getOutMatrix(Matrix)
+     * @see #getOuterMatrix(Matrix)
      */
     private Matrix mOutMatrix = new Matrix();
 
     public DetailImageView(Context context){
         super(context);
+        initView();
     }
 
     public DetailImageView(Context context, AttributeSet set){
         super(context, set);
+        initView();
+    }
+
+    private void initView() {
+        //强制设置图片scaleType为matrix
+        super.setScaleType(ScaleType.MATRIX);
     }
 
     @Override
@@ -54,7 +69,8 @@ public class DetailImageView extends AppCompatImageView {
         @Override
         public boolean onDoubleTap(MotionEvent e) {
             Log.i(TAG, "Double Tap is catched!");
-            return super.onDoubleTap(e);
+            doubleTap(e.getX(), e.getY());
+            return true;
         }
 
         @Override
@@ -62,7 +78,7 @@ public class DetailImageView extends AppCompatImageView {
             if (mClickListener != null){
                 mClickListener.onClick(DetailImageView.this);
             }
-            return true;
+            return super.onSingleTapConfirmed(e);
         }
     });
 
@@ -83,34 +99,151 @@ public class DetailImageView extends AppCompatImageView {
         if (!isReady()) {
             return;
         }
+//        if (getAnimationMatrix() == null){
+//            setAnimationMatrix(mOutMatrix);
+//        }
+        Log.i(TAG, "start calculating animator");
         //获取第一层变换矩阵，从原始图片到适应控件的变换
         Matrix innerMatrix = MathUtils.matrixTake();
         getInnerMatrix(innerMatrix);
         //Matrix对象中有9个float值，其中第0个和第4个分别是x、y的缩放比例
         float innerScale = MathUtils.getMatrixScale(innerMatrix)[0];
         float outerScale = MathUtils.getMatrixScale(mOutMatrix)[0];
-//        float currentScale = innerScale * outerScale;
+        float currentScale = innerScale * outerScale;
+        //控件大小
+        float displayWidth = getWidth();
+        float displayHeight = getHeight();
         //TODO:确定接下来动画的缩放大小nextScale，可能是从小到大或者反过来
         float nextScale = calculateNextScale(innerScale, outerScale);
+        //计算结果矩阵
+        Matrix target = MathUtils.matrixTake(mOutMatrix);
+        //计算所需放缩比例，中心点为指触点
+        target.postScale(nextScale / currentScale, nextScale / currentScale, ex, ey);
+        //将放大点移动到控件中心?TODO:存疑
+        target.postTranslate(displayWidth/ 2f - ex, displayHeight / 2f - ey);
+        //计算放缩之后的图片方框
+        Matrix testM = MathUtils.matrixTake(innerMatrix);
+        testM.postConcat(target);       //进行变换
+        RectF bound = MathUtils.rectFTake(0, 0, getDrawable().getIntrinsicWidth(), getDrawable().getIntrinsicHeight());
+        testM.mapRect(bound);           //rect bound is modified after this mapping
+        //修正位置
+        float postX = 0f;
+        float postY = 0f;
+        if (bound.right - bound.left < displayWidth){
+            postX = (displayWidth - bound.right - bound.left) / 2f;
+        }else if (bound.right < displayWidth){
+            postX = displayWidth - bound.right;
+        }else if (bound.left > 0) {
+            postX = -bound.left;
+        }
+
+        if (bound.bottom - bound.top < displayHeight){
+            postY = (displayHeight - bound.top - bound.bottom) / 2f;
+        }else if (bound.bottom < displayHeight){
+            postY = displayHeight - bound.bottom;
+        }else if (bound.top > 0){
+            postY = -bound.top;
+        }
+
+        target.postTranslate(postX, postY);
+        //停止正在执行的动画
+        cancelAllAnimations();
+//        if (!scale){
+//            scaleValues = new float[]{2,0,0,0,2,0,0,0,1};
+//        }else {
+//            scaleValues = new float[]{1,0,0,0,1,0,0,0,1};
+//        }
+//        scale = !scale;
+//        target = new Matrix();
+//        target.setValues(scaleValues);
+        mAnimator = new ScaleAnimator(mOutMatrix, target);
+        mAnimator.start();
+
+        MathUtils.rectFGiven(bound);
+        MathUtils.matrixGiven(innerMatrix);
+        MathUtils.matrixGiven(testM);
+        MathUtils.matrixGiven(target);
 
 
     }
 
+    private boolean scale = false;
+
+    private float[] scaleValues = new float[]{2,0,0,0,2,0,0,0,1};
+
     private ScaleAnimator mAnimator;
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (isReady()){
+            Log.i(TAG, " is ready!");
+            Matrix matrix = MathUtils.matrixTake();
+            matrix = getCurrentImageMatrix(matrix);
+            setImageMatrix(matrix);
+            MathUtils.matrixGiven(matrix);
+        }
+        super.onDraw(canvas);
+    }
 
     private class ScaleAnimator extends ValueAnimator implements ValueAnimator.AnimatorUpdateListener{
 
+        /**
+         * 开始矩阵
+         */
+        private float[] mStart = new float[9];
+
+        /**
+         * 结束矩阵
+         */
+        private float[] mEnd = new float[9];
+
+        /**
+         * 中间结果矩阵
+         */
+        private float[] mResult = new float[9];
+
+        public ScaleAnimator(Matrix src, Matrix dst){
+            this(src, dst, SCALE_ANIMATOR_DURATION);
+        }
+
+        /**
+         * 构建一个缩放动画
+         * <p>
+         * 从一个矩阵变换到另外一个矩阵
+         *
+         * @param start    开始矩阵
+         * @param end      结束矩阵
+         * @param duration 动画时间
+         */
+        public ScaleAnimator(Matrix start, Matrix end, long duration) {
+            super();
+            setFloatValues(0, 1f);
+            setDuration(duration);
+            addUpdateListener(this);
+            start.getValues(mStart);
+            end.getValues(mEnd);
+            Log.i(TAG, "start: " + Arrays.toString(mStart) + " end: " + Arrays.toString(mEnd));
+        }
 
         @Override
         public void onAnimationUpdate(ValueAnimator animation) {
-
+            //获取动画进度
+            float value = (Float) animation.getAnimatedValue();
+            //根据动画进度计算矩阵中间插值
+            for (int i = 0; i < 9; i++) {
+                mResult[i] = mStart[i] + (mEnd[i] - mStart[i]) * value;
+            }
+            //设置矩阵并重绘
+            mOutMatrix.setValues(mResult);
+            Log.i(TAG, "Matrix: " + Arrays.toString(mResult));
+//            dispatchOuterMatrixChanged();
+            invalidate();
         }
     }
 
     /**
      * 判断当前能否执行手势相关操作，只有当测量、布局等工作完成，获得了控件和Drawable
-     * 最中的参数时，才能执行手势相关操作
+     * 最终的参数时，才能执行手势相关操作
      * 包括：是否有图片，图片是否已经加载，控件大小是否确定？
      * @return 是否已经可以执行手势相关操作
      * @see #doubleTap(float, float)
@@ -126,7 +259,7 @@ public class DetailImageView extends AppCompatImageView {
      * @param matrix
      * @see #doubleTap(float, float)
      */
-    private void getInnerMatrix(Matrix matrix){
+    private Matrix getInnerMatrix(Matrix matrix){
         if (matrix == null){
             matrix = new Matrix();
         }else {
@@ -142,17 +275,37 @@ public class DetailImageView extends AppCompatImageView {
             MathUtils.rectFGiven(dstRect);
             MathUtils.rectFGiven(srcRect);
         }
+        return matrix;
     }
 
     /**
      * 获得外层变换矩阵的值
      * @param matrix
      */
-    private Matrix getOutMatrix(Matrix matrix){
+    private Matrix getOuterMatrix(Matrix matrix){
         if (matrix == null){
             matrix = new Matrix();
         }
         matrix.set(mOutMatrix);
+        return matrix;
+    }
+
+    /**
+     * 获取图片总变换矩阵.
+     * <p>
+     * 总变换矩阵为内部变换矩阵x外部变换矩阵,决定了原图到所见最终状态的变换
+     * 当尚未布局或者原图不存在时,其值无意义.所以在调用前需要确保前置条件有效,否则将影响计算结果.
+     *
+     * @param matrix 用于填充结果的对象
+     * @return 如果传了matrix参数则将matrix填充后返回, 否则new一个填充返回
+     * @see #getOuterMatrix(Matrix)
+     * @see #getInnerMatrix(Matrix)
+     */
+    public Matrix getCurrentImageMatrix(Matrix matrix) {
+        //获取内部变换矩阵
+        matrix = getInnerMatrix(matrix);
+        //乘上外部变换矩阵
+        matrix.postConcat(mOutMatrix);
         return matrix;
     }
 
@@ -170,6 +323,155 @@ public class DetailImageView extends AppCompatImageView {
             return innerScale;
         }else {
             return MAX_SCALE;
+        }
+    }
+
+    /**
+     * 停止所有的手势动画
+     */
+    private void cancelAllAnimations(){
+        if (mAnimator != null){
+            mAnimator.cancel();
+            mAnimator = null;
+        }
+    }
+
+
+    ////////////////////////////////对外广播事件////////////////////////////////
+
+    /**
+     * 外部矩阵变化事件通知监听器
+     */
+    public interface OuterMatrixChangedListener {
+
+        /**
+         * 外部矩阵变化回调
+         * <p>
+         * 外部矩阵的任何变化后都收到此回调.
+         * 外部矩阵变化后,总变化矩阵,图片的展示位置都将发生变化.
+         *
+         * @param detailImageView
+         * @see #getOuterMatrix(Matrix)
+         * @see #getCurrentImageMatrix(Matrix)
+         * @see #getImageBound(RectF)
+         */
+        void onOuterMatrixChanged(DetailImageView detailImageView);
+    }
+
+    /**
+     * 所有OuterMatrixChangedListener监听列表
+     *
+     * @see #addOuterMatrixChangedListener(OuterMatrixChangedListener)
+     * @see #removeOuterMatrixChangedListener(OuterMatrixChangedListener)
+     */
+    private List<OuterMatrixChangedListener> mOuterMatrixChangedListeners;
+
+    /**
+     * 当mOuterMatrixChangedListeners被锁定不允许修改时,临时将修改写到这个副本中
+     *
+     * @see #mOuterMatrixChangedListeners
+     */
+    private List<OuterMatrixChangedListener> mOuterMatrixChangedListenersCopy;
+
+    /**
+     * mOuterMatrixChangedListeners的修改锁定
+     * <p>
+     * 当进入dispatchOuterMatrixChanged方法时,被加1,退出前被减1
+     *
+     * @see #dispatchOuterMatrixChanged()
+     * @see #addOuterMatrixChangedListener(OuterMatrixChangedListener)
+     * @see #removeOuterMatrixChangedListener(OuterMatrixChangedListener)
+     */
+    private int mDispatchOuterMatrixChangedLock;
+
+    /**
+     * 添加外部矩阵变化监听
+     *
+     * @param listener
+     */
+    public void addOuterMatrixChangedListener(OuterMatrixChangedListener listener) {
+        if (listener == null) {
+            return;
+        }
+        //如果监听列表没有被修改锁定直接将监听添加到监听列表
+        if (mDispatchOuterMatrixChangedLock == 0) {
+            if (mOuterMatrixChangedListeners == null) {
+                mOuterMatrixChangedListeners = new ArrayList<OuterMatrixChangedListener>();
+            }
+            mOuterMatrixChangedListeners.add(listener);
+        } else {
+            //如果监听列表修改被锁定,那么尝试在监听列表副本上添加
+            //监听列表副本将会在锁定被解除时替换到监听列表里
+            if (mOuterMatrixChangedListenersCopy == null) {
+                if (mOuterMatrixChangedListeners != null) {
+                    mOuterMatrixChangedListenersCopy = new ArrayList<OuterMatrixChangedListener>(mOuterMatrixChangedListeners);
+                } else {
+                    mOuterMatrixChangedListenersCopy = new ArrayList<OuterMatrixChangedListener>();
+                }
+            }
+            mOuterMatrixChangedListenersCopy.add(listener);
+        }
+    }
+
+    /**
+     * 删除外部矩阵变化监听
+     *
+     * @param listener
+     */
+    public void removeOuterMatrixChangedListener(OuterMatrixChangedListener listener) {
+        if (listener == null) {
+            return;
+        }
+        //如果监听列表没有被修改锁定直接在监听列表数据结构上修改
+        if (mDispatchOuterMatrixChangedLock == 0) {
+            if (mOuterMatrixChangedListeners != null) {
+                mOuterMatrixChangedListeners.remove(listener);
+            }
+        } else {
+            //如果监听列表被修改锁定,那么就在其副本上修改
+            //其副本将会在锁定解除时替换回监听列表
+            if (mOuterMatrixChangedListenersCopy == null) {
+                if (mOuterMatrixChangedListeners != null) {
+                    mOuterMatrixChangedListenersCopy = new ArrayList<OuterMatrixChangedListener>(mOuterMatrixChangedListeners);
+                }
+            }
+            if (mOuterMatrixChangedListenersCopy != null) {
+                mOuterMatrixChangedListenersCopy.remove(listener);
+            }
+        }
+    }
+
+
+    /**
+     * 触发外部矩阵修改事件
+     * <p>
+     * 需要在每次给外部矩阵设置值时都调用此方法.
+     *
+     * @see #mOutMatrix
+     */
+    private void dispatchOuterMatrixChanged() {
+        if (mOuterMatrixChangedListeners == null) {
+            return;
+        }
+        //增加锁
+        //这里之所以用计数器做锁定是因为可能在锁定期间又间接调用了此方法产生递归
+        //使用boolean无法判断递归结束
+        mDispatchOuterMatrixChangedLock++;
+        //在列表循环过程中不允许修改列表,否则将引发崩溃
+        for (OuterMatrixChangedListener listener : mOuterMatrixChangedListeners) {
+            listener.onOuterMatrixChanged(this);
+        }
+        //减锁
+        mDispatchOuterMatrixChangedLock--;
+        //如果是递归的情况,mDispatchOuterMatrixChangedLock可能大于1,只有减到0才能算列表的锁定解除
+        if (mDispatchOuterMatrixChangedLock == 0) {
+            //如果期间有修改列表,那么副本将不为null
+            if (mOuterMatrixChangedListenersCopy != null) {
+                //将副本替换掉正式的列表
+                mOuterMatrixChangedListeners = mOuterMatrixChangedListenersCopy;
+                //清空副本
+                mOuterMatrixChangedListenersCopy = null;
+            }
         }
     }
 
